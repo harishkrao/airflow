cd67a5159523e2614677938b64374adceb033b1d
Harish Kesava Rao <harish.kesavarao@databricks.com>
2023-01-08 22:58:57 -0800

    Unit test for new events

--- tests/providers/databricks/sensors/test_databricks.py
+++ tests/providers/databricks/sensors/test_databricks.py
@@ -17,14 +17,54 @@
 # under the License.
 #
 from __future__ import annotations
+import unittest
+from unittest import mock
+
+from airflow.providers.databricks.sensors.databricks import DatabricksSQLSensor
+# from databricks.sql.types import Row
 
 from datetime import datetime, timedelta
 
+TASK_ID = "db-sensor"
 DEFAULT_CONN_ID = "databricks_default"
+DEFAULT_SCHEMA = "schema1"
+DEFAULT_CATALOG = "catalog1"
+DEFAULT_TABLE = "table1"
 
 TIMESTAMP_TEST = datetime.now() - timedelta(days=30)
 
-# class TestDatabricksSQLSensor(unittest.TestCase):
-#
-#     @mock.path('airflow.providers.databricks.sensors.databricks.DatabricksSQLSensor')
-#     def test_exec_
+
+class TestDatabricksSQLSensor(unittest.TestCase):
+    @mock.patch("airflow.providers.databricks.sensors.databricks.DatabricksSqlHook")
+    def test_exec_success_sensor_find_new_events(self, db_mock_class):
+        sensor = DatabricksSQLSensor(
+            task_id=TASK_ID,
+            databricks_conn_id=DEFAULT_CONN_ID,
+            schema=DEFAULT_SCHEMA,
+            catalog=DEFAULT_CATALOG,
+            db_sensor_type="table_changes",
+            table_name=DEFAULT_TABLE,
+            timestamp=TIMESTAMP_TEST,
+        )
+
+        db_mock = db_mock_class.return_value
+        db_mock.get_table_version.return_value = 123
+
+        with mock.patch("airflow.providers.databricks.sensors.databricks.DatabricksSQLSensor") as mock_sensor:
+            mock_sensor.get_previous_version.return_value = 122
+            mock_sensor.set_version.return_value = None
+            result = sensor.poke(None)
+            assert result
+
+        db_mock_class.assert_called_once_with(
+            databricks_conn_id="databricks_default",
+            catalog="catalog1",
+            schema="schema1",
+            table_name="table1",
+            db_sensor_type="table_changes",
+            timestamp=datetime.now() - timedelta(days=30),
+        )
+
+        db_mock.get_table_version.assert_called_once_with(
+            DEFAULT_CONN_ID, DEFAULT_CATALOG, DEFAULT_TABLE, DEFAULT_SCHEMA
+        )
